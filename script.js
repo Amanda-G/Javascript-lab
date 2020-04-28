e5233116aae80f1eb591526041d5e9ff76e2e389
Amanda-G <amanda.gunturu@yahoo.com>
2020-04-28 09:26:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const name = "Amanda Gunturu";
+const age = 26;
+const birthday = "October 17th";
+const detroitGC = true;
+const lifeEvents = [
+    "I was born in Warren, MI",
+    "I grew up in Brown City, MI",
+    "I have a blue beta named Bubbles",
+    "When I was 11 I got my finger smashed in a door while fighting with my sister."
+]
+
+if (detroitGC) {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`)
+} else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`)
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+}
+
+let counter = 0;
+
+while (true) {
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    if (randomNumber != 5) {
+        counter++
+        console.log(`${randomNumber} !==5`)
+    } else {
+        counter++
+        console.log(`${randomNumber} === 5. It took ${counter} iterations to randomly generate the number 5.`);
+        break;
+    }
+
+}
